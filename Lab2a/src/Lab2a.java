afc687983f545acfce59f6d0aa509f677d32b249
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-01-26 17:19:21 -0500

    half of part 1 done

---
+++ Lab2a/src/Lab2a.java
@@ -0,0 +1,19 @@
+
+public class Lab2a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declaring Variables & Assigning Values
+		byte byt = 'J';
+		short shrt = 16;
+		int nt = 4;
+		long lng = 64;
+		float flt = (float) 4.7;
+		double dbl = 3.3;
+		char chr = 'l';
+		String str = new String("I happy");
+		boolean bool = false;
+
+	}
+
+}
