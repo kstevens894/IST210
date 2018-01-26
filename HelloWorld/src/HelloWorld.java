aeb8d2301be77975344424e7cac8a7c1465daa0f
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-01-26 17:05:08 -0500

    Hello World Test

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Printing Hello World
+		System.out.println("Hello world!");
+
+	}
+
+}
