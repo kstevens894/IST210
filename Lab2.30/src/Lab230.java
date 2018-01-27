539e0b319c380d25c654931cc7881bba5b08efbf
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-01-27 14:04:05 -0500

    LAb2.30 done

--- Lab2.30/src/Lab230.java
+++ Lab2.30/src/Lab230.java
@@ -14,14 +14,16 @@ public class Lab230 {
 		System.out.print("Enter a 5 digit number: ");
 		//Store Users input
 	    digitInput = input.nextInt();
+	  //Calculations to pull each digit seperately//
 	    digit1 = digitInput / 10000;
 	    digit2 = digitInput % 10000 / 1000;
 	    digit3 = digitInput % 1000 / 100;
 	    digit4 = digitInput % 100 / 10;
 	    digit5 = digitInput % 10;
-	    System.out.printf("Digits in %d are %d %d %d %d %d", digitInput, digit1, digit2, digit3, digit4, digit5);
-		//Calculations to pull each digit seperately//
-		
+	    //Print every single digit out//
+	    System.out.printf("Digits in %d are %d %d %d %d %d", digitInput, digit1, digit2, digit3, digit4, digit5);		
 	}
-
 }
+			//OUTPUT ANSWER//
+		/*Enter a 5 digit number: 97641
+		Digits in 97641 are 9 7 6 4 1*/
