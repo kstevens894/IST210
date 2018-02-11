a9835e471411e36ea3ac1fd80f8965790c8c5c8a
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-02-11 15:27:43 -0500

    LAB 5.25 DONE

---
+++ 5.24Diamond/src/Diamond.java
@@ -0,0 +1,37 @@
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		int spaces = 4, a = 1;
+		for(int lines = 1; lines <= 5; lines++)
+		{
+			for( int w = 1; w <= spaces; w++)
+			{
+				System.out.print(" ");
+			}
+			for( int w = 1; w <= a; w++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			spaces--;
+			a +=2;
+		}
+		a = 7;
+		spaces = 1;
+		for(int lines = 1; lines <= 4; lines++)
+		{
+			for( int w = 1; w <= spaces; w++)
+			{
+				System.out.print(" ");
+			}
+			for( int w = 1; w <= a; w++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			spaces++;
+			a -=2;
+		}  
+	}
+}
