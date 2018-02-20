f1a4fe80da133948fea8c5a923547086da5d62ae
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-02-19 20:18:38 -0500

    Lab 6.33 C# done

---
+++ Stevens C craps/CrapsGame.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stevens_C_craps
+{
+    enum Status {  CONTINUE, WON, LOST}
+
+    enum DiceNames
+    {
+        SNAKE_EYES = 2,
+        TREY = 3,
+        SEVEN = 7,
+        YO_LEVEN = 11,
+        BOX_CARS = 12
+    }
+    class CrapsGame
+    {
+        public Status gameStatus { get; set; }
+
+        private decimal balance;
+        public decimal Balance
+        {
+            get { return balance; }
+            set { if (value >= 0) balance = value; }
+        }
+
+        private decimal wager;
+        public decimal Wager
+        {
+            get { return wager;}
+            set { wager = value; }
+        }
+
+        public CrapsGame()
+        {
+            Balance = 1000;
+            gameStatus = Status.CONTINUE;
+        }
+
+        public void Play()
+        {
+            int myPoint = 0;
+            int sumOfDice = RollDice();
+            switch ((DiceNames)sumOfDice)
+            {
+                case DiceNames.SEVEN:
+                case DiceNames.YO_LEVEN:
+                    gameStatus = Status.WON;
+                    break;
+                case DiceNames.SNAKE_EYES:
+                case DiceNames.TREY:
+                case DiceNames.BOX_CARS:
+                    gameStatus = Status.LOST;
+                    break;
+                default:
+                    gameStatus = Status.CONTINUE;
+                    myPoint = sumOfDice;
+                    Console.WriteLine("Point is {0}", myPoint);
+                    break;
+            }
+            while (gameStatus == Status.CONTINUE)
+            {
+                sumOfDice = RollDice();
+
+                if (sumOfDice == myPoint)
+                    gameStatus = Status.WON;
+                else if (sumOfDice == (int)DiceNames.SEVEN)
+                    gameStatus = Status.LOST;
+            }
+
+            if(gameStatus == Status.WON)
+            {
+                Balance += Wager;
+                Console.WriteLine("Congratulations!! You won. Your balance is {0:C}", Balance);
+            }
+            else
+            {
+                Console.WriteLine("Player losses");
+                Balance -= Wager;
+                if (Balance > 0)
+                {
+                    Console.WriteLine("Sorry, you lost, your balance is {):C}", Balance);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, your are busted, your balance is {0:C}", Balance);
+                }
+            }
+            return;
+        }
+        public int RollDice()
+        {
+            Random randomNumbers = randomNumbers = new Random();
+
+            int die1 = randomNumbers.Next(1, 7);
+            int die2 = randomNumbers.Next(1, 7);
+
+            int sum = die1 + die2;
+
+            Console.WriteLine("Player rolled {0} + {1} = {2}",
+                die1, die2, sum);
+            return sum;
+        }
+    }
+}
+
---
+++ Stevens C craps/program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stevens_C_craps
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            CrapsGame game = new CrapsGame();
+
+            do
+            {
+                Console.Write("\nWould you like to pllay the game of craps: [y/n]");
+                char response = Console.ReadLine().ToLower()[0];
+                if (!response.Equals('y'))
+                    break;
+                Console.WriteLine("Enter Wager or -1 to Quit. Your current balance is {0:C}: ", game.Balance);
+                game.Wager = Convert.ToDecimal(Console.ReadLine());
+                if (game.Wager == -1)
+                    break;
+                game.Wager = ValidateWager(game.Wager, game.Balance);
+                if (game.Wager == -1)
+                    break;
+                game.Play();
+                if (game.Balance <= 0)
+                    break;
+            } while (true);
+            Console.WriteLine("Good Bye...");
+        }
+
+        private static decimal ValidateWager(decimal wag, decimal bal)
+        {
+            decimal newWager = wag;
+            while (newWager <= 0 || newWager > bal)
+            {
+                Console.Write("{0} is invalid. Wager must be greater than - and it cannot exceed balance: {1:C} ", newWager, bal);
+                newWager = Convert.ToDecimal(Console.ReadLine());
+                if (wag == -1)
+                    break;
+            }
+            return newWager;
+        }
+    }
+}
+/*
+Would you like to pllay the game of craps: [y/n]y
+Enter Wager or -1 to Quit. Your current balance is $1,000.00:
+300
+Player rolled 5 + 3 = 8
+Point is 8
+Player rolled 5 + 3 = 8
+Congratulations!! You won. Your balance is $1,300.00
+
+Would you like to pllay the game of craps: [y/n]y
+Enter Wager or -1 to Quit. Your current balance is $1,300.00:
+100
+Player rolled 5 + 1 = 6
+Point is 6
+Player rolled 5 + 1 = 6
+Congratulations!! You won. Your balance is $1,400.00
+
+Would you like to pllay the game of craps: [y/n]y
+Enter Wager or -1 to Quit. Your current balance is $1,400.00:
+500
+Player rolled 3 + 5 = 8
+Point is 8
+Player rolled 3 + 5 = 8
+Congratulations!! You won. Your balance is $1,900.00
+
+Would you like to pllay the game of craps: [y/n]y
+Enter Wager or -1 to Quit. Your current balance is $1,900.00:
+300
+Player rolled 2 + 5 = 7
+Congratulations!! You won. Your balance is $2,200.00
+
+Would you like to pllay the game of craps: [y/n]y
+Enter Wager or -1 to Quit. Your current balance is $2,200.00:
+310
+Player rolled 2 + 6 = 8
+Point is 8
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 5 + 4 = 9
+Player rolled 4 + 1 = 5
+Player rolled 4 + 1 = 5
+Player rolled 4 + 1 = 5
+Player rolled 4 + 1 = 5
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 6 + 5 = 11
+Player rolled 5 + 1 = 6
+Player rolled 5 + 1 = 6
+Player rolled 5 + 1 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 1 + 5 = 6
+Player rolled 3 + 3 = 6
+Player rolled 3 + 3 = 6
+Player rolled 3 + 3 = 6
+Player rolled 2 + 6 = 8
+Congratulations!! You won. Your balance is $2,510.00
+
+Would you like to pllay the game of craps: [y/n]n
+Good Bye...
+Press any key to continue . . .*/
