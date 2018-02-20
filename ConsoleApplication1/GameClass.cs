c11c6a5a95fabe3ed5e60b15bad5c03a39db9eac
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-02-19 19:45:35 -0500

    Lab 6.31#

---
+++ ConsoleApplication1/GameClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class GameClass
+    {
+        private static Random rand;
+        private int secretNumber;
+        private int guess;
+        private int numberOfTries;
+
+        public GameClass()
+        {
+            rand = new Random();
+            secretNumber = 0;
+            guess = 0;
+        }
+        public void playGame()
+        {
+            secretNumber = GetRandomNumber();
+            do
+            {
+                numberOfTries++;
+                guess = promptAndGetResponse();
+                if (guess == -1)
+                {
+                    Console.WriteLine("Good Bye...");
+                    return;
+                }
+                bool gameOver = CheckStatus(secretNumber, guess);
+                if (gameOver)
+                    break;
+            } while (true);
+        }
+        private bool CheckStatus (int secretnumber, int guess)
+        {
+            bool gameOver = false;
+            if (secretNumber == guess)
+            {
+                if (numberOfTries == 10) Console.WriteLine("Aha! You know the scret!");
+                else if (numberOfTries > 10) Console.WriteLine("You should be able to do better!");
+                else Console.WriteLine("Either you know the scret or you got lucky");
+                gameOver = true;
+            }
+            else if (secretNumber < guess)
+            {
+                Console.WriteLine($"{guess} - Guess lower...");
+            }
+            else
+            {
+                Console.WriteLine($"{guess} - Guess higher...");
+            }
+            return gameOver;
+        }
+        private int promptAndGetResponse()
+        {
+            Console.WriteLine("Please enter a guess: [1]1000]: ");
+            return int.Parse(Console.ReadLine());
+        }
+        private int GetRandomNumber()
+        {
+            return rand.Next(1, 1001);
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Would you like to play a guessing game? [y/n]: ");
+                char response = Console.ReadLine().ToLower()[0];
+                if (response.Equals('y'))
+                {
+                    GameClass game = new GameClass();
+                    game.playGame();
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);Console.WriteLine("Good bye. Please come again...");
+        }
+    }
+}
