bd16ea10479e56f9ce72824e0425eed384c7d500
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-02-19 19:49:52 -0500

    LAB 6.31 done

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -27,3 +27,44 @@ namespace ConsoleApplication1
         }
     }
 }
+/*
+Would you like to play a guessing game?[y / n]: y
+ Please enter a guess: [1]1000]:
+400
+400 - Guess higher...
+Please enter a guess: [1]1000]:
+800
+800 - Guess lower...
+Please enter a guess: [1]1000]:
+600
+600 - Guess lower...
+Please enter a guess: [1]1000]:
+500
+500 - Guess higher...
+Please enter a guess: [1]1000]:
+550
+550 - Guess lower...
+Please enter a guess: [1]1000]:
+545
+545 - Guess lower...
+Please enter a guess: [1]1000]:
+530
+530 - Guess lower...
+Please enter a guess: [1]1000]:
+520
+520 - Guess lower...
+Please enter a guess: [1]1000]:
+510
+510 - Guess higher...
+Please enter a guess: [1]1000]:
+515
+515 - Guess lower...
+Please enter a guess: [1]1000]:
+513
+513 - Guess lower...
+Please enter a guess: [1]1000]:
+511
+You should be able to do better!
+Would you like to play a guessing game?[y / n]: n
+ Good bye.Please come again...
+Press any key to continue . . .*/
