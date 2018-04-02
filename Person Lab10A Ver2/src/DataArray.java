b8e3bd6580b0080884ef34ede42503c060e86208
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-04-02 12:12:04 -0400

    PErson class v2

---
+++ Person Lab10A Ver2/src/DataArray.java
@@ -0,0 +1,96 @@
+
+public class DataArray {
+	private Person[] a;
+	private int nElements;
+	
+	public DataArray(int max) {
+		a = new Person[max];
+		nElements = 0;
+	}
+	
+	public void insert(String last, String first, int age) 
+	{
+		a[nElements] = new Person(last, first, age);
+		nElements++;
+	}
+	
+	public void displayAll()
+	{
+		for(int m = 0; m < nElements; m++) {
+			a[m].displayPerson();
+		}
+	}
+	public Person Find(String searchName)
+	{
+		int k;
+		for (k = 0; k < nElements; k++) {
+			if (a[k].getLast().equals(searchName))
+				break;
+		}
+		
+		if (k == nElements) {
+			return null;
+		}
+		else {
+			return a[k];
+		}
+	}
+	public Person Find1(String otherPerson)
+	{
+		int j;
+		for  (j = 0; j < nElements; j++)
+		{
+			if (a[j].equals(otherPerson))
+				break;
+		}
+		
+		if (j == nElements)
+		{
+			return null;
+		}
+		else
+		{
+			return a[j];
+		}
+		
+	}
+	
+	public void bubbleSort()
+	{
+		int outter, inner;
+		Person temp;
+		
+		for (outter = nElements - 1; outter > 0; outter--)
+		{
+			for (inner = 0; inner < outter; inner++)
+			{
+				if(a[inner].compareTo(a[inner + 1]) > 0)
+				{
+					temp = a[inner];
+					a[inner] = a[inner + 1];
+					a[inner + 1] = temp; 
+				}
+			}
+		}
+	}
+	public boolean delete(String searchName) {
+		int k;
+		for (k = 0; k < nElements; k++) {
+			if (a[k].getLast().equals(searchName))
+				break;
+		}
+		if (k == nElements) {
+			return false;
+		}
+		else {
+			for(int w = k; w < nElements; w++)
+			{
+				a[w] = a[w + 1];
+			}
+			
+			nElements--;
+			return true;
+		}
+	}
+	
+}
---
+++ Person Lab10A Ver2/src/PersonMain.java
@@ -0,0 +1,174 @@
+import java.util.Scanner;
+
+public class PersonMain {
+	public static void main(String[] args) {
+		int maxSize = 100;
+		Scanner input = new Scanner(System.in);
+		DataArray arr;
+		arr = new DataArray(maxSize);
+		
+		arr.insert("Nichols","Paul", 29);
+		arr.insert("Steven", "Bob", 50);
+		arr.insert("Ming", "Dave", 44);
+		arr.insert("Williams", "Kyle", 10);
+		arr.insert("Donovan","Michael", 19);
+		arr.insert("Quin", "Adam", 34);
+		arr.insert("Coke", "Pepsi", 80);
+		arr.insert("Apple", "Red", 20);
+		arr.insert("Kyles", "Dan", 31);
+		arr.insert("Johns", "Brett", 49);
+		
+		System.out.println();
+		System.out.print("-----------------Display All-----------------");
+		System.out.println();
+		arr.displayAll();
+		System.out.println();
+		
+		//-----------FIND--------------------------//
+		System.out.print("-----------------FIND-----------------\n");
+		System.out.print("Find a person: ");
+		
+		String searchKey = "Williams";
+		Person personFound;
+		personFound = arr.Find(searchKey);
+		System.out.println();
+		if (personFound != null)
+		{
+			System.out.print("Found ");
+			personFound.displayPerson();
+		}
+		else
+		{
+			System.out.print("Can't find " + searchKey);
+		}
+		System.out.println();
+		String searchKey2 ="Jill";
+		personFound = arr.Find1(searchKey2);
+		System.out.println();
+		if (personFound != null)
+		{
+			System.out.print("Found ");
+			personFound.displayPerson();
+		}
+		else
+		{
+			System.out.print("Can't find " + searchKey2);
+		}
+		System.out.println();
+		//-------------Delete---------------------------//
+		
+		System.out.print("Delete Steven, Ming, and Williams\n");
+		if (arr.delete("Steven")){
+			System.out.println();
+			System.out.print("Successfully deleted \"Steven\"");
+		}
+		
+		else {
+			System.out.println();
+			System.out.print("Not found. Could not delete \"Steven\"");
+		}
+		if (arr.delete("Ming")){
+			System.out.println();
+			System.out.print("Successfully deleted \"Min\"");
+		}
+		else {
+			System.out.println();
+			System.out.print("Not found. Could not delete \"Ming\"");
+		}
+		if (arr.delete("Dang")){
+			System.out.println();
+			System.out.print("Successfully deleted \"Dang\"");
+		}
+		else {
+			System.out.println();
+			System.out.print("Not found. Could not delete \"Dang\"");
+		}
+		System.out.println();
+		System.out.println();
+		
+		System.out.print("Display all after deleting, Smith, Yee, and Williams.\n\n");
+		arr.displayAll();
+		System.out.println();
+		System.out.println();
+		
+		//-------------------------INSERT---------------------------//
+		
+		String lastName, firstName;
+		int age;
+		
+		System.out.println("Insert: Please enter firstName: ");
+		firstName = input.nextLine();
+		
+		System.out.println("Insert: Please enter lastName: ");
+		lastName = input.nextLine();
+		
+		System.out.println("Insert: Please enter age: ");
+		age = input.nextInt();
+		System.out.println();
+		
+		arr.insert(lastName, firstName, age);
+		System.out.printf("Enter Successfully %s, %s, %d%n", firstName, lastName, age);
+		
+		System.out.print("---------Display all persons---------\n");
+		arr.bubbleSort();
+		arr.bubbleSort();
+		arr.displayAll();
+}
+}
+/*
+-----------------Display All-----------------
+
+-----------------Display All-----------------
+Last Name: Nichols, First Name: Paul, Age: 29
+Last Name: Steven, First Name: Bob, Age: 50
+Last Name: Ming, First Name: Dave, Age: 44
+Last Name: Williams, First Name: Kyle, Age: 10
+Last Name: Donovan, First Name: Michael, Age: 19
+Last Name: Quin, First Name: Adam, Age: 34
+Last Name: Coke, First Name: Pepsi, Age: 80
+Last Name: Apple, First Name: Red, Age: 20
+Last Name: Kyles, First Name: Dan, Age: 31
+Last Name: Johns, First Name: Brett, Age: 49
+
+-----------------FIND-----------------
+Find a person: 
+Found Last Name: Williams, First Name: Kyle, Age: 10
+
+
+Can't find Jill
+Delete Steven, Ming, and Williams
+
+Successfully deleted "Steven"
+Successfully deleted "Min"
+Not found. Could not delete "Dang"
+
+Display all after deleting, Smith, Yee, and Williams.
+
+Last Name: Nichols, First Name: Paul, Age: 29
+Last Name: Williams, First Name: Kyle, Age: 10
+Last Name: Donovan, First Name: Michael, Age: 19
+Last Name: Quin, First Name: Adam, Age: 34
+Last Name: Coke, First Name: Pepsi, Age: 80
+Last Name: Apple, First Name: Red, Age: 20
+Last Name: Kyles, First Name: Dan, Age: 31
+Last Name: Johns, First Name: Brett, Age: 49
+
+
+Insert: Please enter firstName: 
+Frank
+Insert: Please enter lastName: 
+Simmons
+Insert: Please enter age: 
+74
+
+Enter Successfully Frank, Simmons, 74
+---------Display all persons---------
+Last Name: Apple, First Name: Red, Age: 20
+Last Name: Coke, First Name: Pepsi, Age: 80
+Last Name: Donovan, First Name: Michael, Age: 19
+Last Name: Johns, First Name: Brett, Age: 49
+Last Name: Kyles, First Name: Dan, Age: 31
+Last Name: Nichols, First Name: Paul, Age: 29
+Last Name: Quin, First Name: Adam, Age: 34
+Last Name: Simmons, First Name: Frank, Age: 74
+Last Name: Williams, First Name: Kyle, Age: 10*/
