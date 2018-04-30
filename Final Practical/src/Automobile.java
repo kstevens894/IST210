9d9eb70733f94bbb5ebca2380eed4812c4956f50
kstevens894 <31905156+kstevens894@users.noreply.github.com>
2018-04-30 19:31:26 -0400

    Implementing method to return NORMAL now

--- Final Practical/src/Automobile.java
+++ Final Practical/src/Automobile.java
@@ -1,4 +1,10 @@
 
 public class Automobile extends Vehicle implements VehicleDoor{
 	public Automobile() {super("honk honk");}
+	
+	public Automobile(String hornSound){ super(hornSound); }
+	
+	public String getDoorType() {
+		return DoorType.NORMAL;
+	}
 }
